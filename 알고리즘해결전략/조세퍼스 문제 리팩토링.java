91d3ddafa3a94a6f6621960be46967165ff23fad
SoongsilMilhouse <rlawldyd577@gmail.com>
2018-09-06 20:09:34 +0900

    20180906

---
+++ 알고리즘해결전략/조세퍼스 문제 리팩토링.java
@@ -0,0 +1,39 @@
+﻿import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Josephus_refactored {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int C, N, K;
+
+        C = sc.nextInt();
+
+        for (int i = 0; i < C; i++) {
+            List list = new LinkedList();
+            int kill = 0;
+
+            N = sc.nextInt();
+            K = sc.nextInt();
+            setList(list, N);
+
+            while (list.size() > 2) {
+                list.remove(kill);
+                if (kill == list.size()) kill = 0;
+
+                for (int j = 0; j < K - 1; j++) {
+                    kill++;
+                    if (kill == list.size()) kill = 0;
+                }
+            }
+            System.out.println(list);
+        }
+        sc.close();
+    }
+
+    public static void setList(List list, int N) {
+        for (int i = 1; i <= N; i++) {
+            list.add(i);
+        }
+    }
+}
