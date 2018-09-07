6a65d528488944793c6baf131473978a0765f710
SoongsilMilhouse <rlawldyd577@gmail.com>
2018-09-07 19:39:40 +0900

    20180907

---
+++ 알고리즘해결전략/짝이 맞지 않는 괄호.java
@@ -0,0 +1,32 @@
+﻿import java.util.Scanner;
+import java.util.Stack;
+
+public class Brackets2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCase;
+
+        testCase = sc.nextInt();
+
+        for(int i = 0; i < testCase; i++) {
+            Stack stack = new Stack();
+            String[] brackets = sc.next().split("");
+
+            for(int j = 0; j < brackets.length; j++) {
+                if(brackets[j].equals("(") || brackets[j].equals("{") || brackets[j].equals("[")) {
+                    stack.push(brackets[j]);
+                } else {
+                    if((brackets[j].equals(")") && stack.peek().equals("(")) || (brackets[j].equals("}") && stack.peek().equals("{")) || (brackets[j].equals("]") && stack.peek().equals("[")) )
+                        stack.pop();
+                    else
+                        stack.push(brackets[j]);
+                }
+            }
+            
+            if(!stack.isEmpty())
+                System.out.println("NO");
+            else
+                System.out.println("YES");
+        }
+    }
+}
